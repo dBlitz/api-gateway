61fd1a72fb770df0cc30c588ef5a3f4572199ae6
DBlitz <dblitz@DBlitzs-MBP.attlocal.net>
2020-04-22 19:49:45 -0500

    no errors but need to figure out how to get body response of http node.js request

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -20,8 +20,8 @@ async function getUsers_All(data) {
 }
 
 async function getUser_ByID(data) {
-    var token = JSON.parse(data).token;
-    var decoded_token = await jwt.verify(token, secret_key, function(err, decoded) {
+  var token = JSON.parse(data).token;
+  var decoded_token = await jwt.verify(token, secret_key, function(err, decoded) {
     if ( err ) {
       throw new Error('invalid Authorization');
     }
@@ -32,23 +32,21 @@ async function getUser_ByID(data) {
 
       // return decoded_token;
 
-    console.log("decoded???" + decoded_token.user)
+      console.log("decoded???" + decoded_token.user)
       return new Promise(function (resolve, reject) {
-
-      http.get({
-        hostname: 'localhost',
-        port: 1000,
-        path: '/' + decoded_token.user,
+        http.get({
+          hostname: 'localhost',
+          port: 1000,
+          path: '/' + decoded_token.user,
         agent: false  // Create a new agent just for this one request
       }, (res) => {
-        // console.log(res)
-        resolve("yo" + res);
+        console.log(res)
+        resolve("hey");
         // Do stuff with response
       });
 
       });
-
-
+    }
 
  //      return new Promise(function (resolve, reject) {
  //   request.get({url:'http://localhost:1000/api/users/user/' + decoded_token.user.toString(), 
@@ -57,14 +55,13 @@ async function getUser_ByID(data) {
  //        resolve(body)
  //      /* ... */ })
  // });
-}
 
-async function createUser(theUser) {
- const created_user =  await user_service.createUser(theUser);
- return created_user;
-}
+ async function createUser(theUser) {
+   const created_user =  await user_service.createUser(theUser);
+   return created_user;
+ }
 
-async function login(user) {
+ async function login(user) {
   return new Promise(function (resolve, reject) {
    request.post({url:'http://localhost:1000/api/users/login', 
     form: {email_address: user.email_address, 
