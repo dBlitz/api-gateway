37da73a1703c97adddc91bff9033d91e99985636
DBlitz <dblitz@DBlitzs-MBP.attlocal.net>
2020-04-22 20:14:32 -0500

    cleaned get user by id up

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -32,48 +32,24 @@ async function getUser_ByID(data) {
   return new Promise(function (resolve, reject) {
 
     request('http://localhost:1000/api/users/user/' + decoded_token.user, function (error, response, body) {
-    console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+    // console.error('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
     resolve(JSON.parse(body));
   });
- // http.get({
- //          hostname: 'localhost',
- //          port: 1000,
- //          path: '/api/users/user/' + decoded_token.user,
- //        agent: false  // Create a new agent just for this one request
- //      }, (res) => {
- //        console.log(res.body)
- //        resolve(res.body);
- //        // Do stuff with response
- //      });
 
 
   });
+}
 
-      // return decoded_token;
 
-      // console.log("decoded???" + decoded_token.user)
-      // return new Promise(function (resolve, reject) {
-      // //  
 
-      // });
-    }
+async function createUser(theUser) {
+ const created_user =  await user_service.createUser(theUser);
+ return created_user;
+}
 
- //      return new Promise(function (resolve, reject) {
- //   request.get({url:'http://localhost:1000/api/users/user/' + decoded_token.user.toString(), 
- //    form: {email_address: user.email_address, 
- //      password: user.password}}, function(err,httpResponse,body) { 
- //        resolve(body)
- //      /* ... */ })
- // });
-
- async function createUser(theUser) {
-   const created_user =  await user_service.createUser(theUser);
-   return created_user;
- }
-
- async function login(user) {
+async function login(user) {
   return new Promise(function (resolve, reject) {
    request.post({url:'http://localhost:1000/api/users/login', 
     form: {email_address: user.email_address, 
