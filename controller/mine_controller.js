fdee0bc1a78dcc9a49fcaaef31f1a2467551978a
DBlitz <dblitz@DBlitzs-MBP.attlocal.net>
2020-04-26 15:46:10 -0500

    removed unnecessary console.logs

--- controller/mine_controller.js
+++ controller/mine_controller.js
@@ -7,10 +7,7 @@ var secret_key = require('../database/config/auth/secret_key').SECRET_KEY.toStri
 var http = require('http');
 
 async function createMine(authorized_user, the_mine) {
-
-  console.log(authorized_user)
   var user_fks = [authorized_user.user]
-  console.log(authorized_user.user)
   return new Promise(function (resolve, reject) {
    request.post({url:'http://localhost:1100/api/mine/create', 
     form: {mine_name: the_mine.mine_name, fk_user_ids: JSON.stringify([authorized_user.user])
@@ -43,10 +40,6 @@ async function getMines_ByUserID(authorized_user) {
 }
 
 async function updateMine(authorized_user, the_mine) {
-
-  console.log(authorized_user)
-  var user_fks = [authorized_user.user]
-  console.log(authorized_user.user)
   return new Promise(function (resolve, reject) {
    request.post({url:'http://localhost:1100/api/mine/update', 
     form: { id: the_mine.id, mine_name: the_mine.mine_name }, json:true
